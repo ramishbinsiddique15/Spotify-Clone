a17a140aa59c503fcdf3eee729bd7ae116e666fd
Ramish Bin Siddique <ramishbinsiddique24@gmail.com>
2024-07-02 14:42:11 +0500

    Optimized project for vercel deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connection = mongoose.connect("mongodb://localhost:27017/Login");
+const connection = mongoose.connect("mongodb+srv://rbs24:15102004rbs@halalspotify.utv9f8k.mongodb.net/?retryWrites=true&w=majority&appName=halalspotify");
 
 connection
   .then(() => {
